a501a7c2d985c5170d4e3f0671ad932dd135ca96
pyo_16 <94816219+pyowonsik@users.noreply.github.com>
2024-10-01 13:37:42 +0900

    hotfix : modify tab menu text -> image

--- app/src/main/java/com/wspyo/ondootdo/MainFragment.kt
+++ app/src/main/java/com/wspyo/ondootdo/MainFragment.kt
@@ -78,7 +78,7 @@ class MainFragment : Fragment() {
             temperature in 17.0..19.9 -> listOf("얇은 니트", "맨투맨", "가디건", "청바지")
             temperature in 12.0..16.9 -> listOf("자켓", "가디건", "야상", "스타킹", "청바지", "면바지")
             temperature in 9.0..11.9 -> listOf("자켓", "트렌치코트", "야상", "니트", "청바지", "스타킹")
-            temperature in 5.0..8.9 -> listOf("코트", "가죽자켓", "히트텍", "니트", "레깅스")
+            temperature in 5.0..8.9 -> listOf("코트", "가죽자켓", "내복", "니트", "레깅스")
             else -> listOf("패딩", "두꺼운코트", "목도리", "기모제품")
         }
         binding.WearTextView.text = outfit.joinToString(", ")
