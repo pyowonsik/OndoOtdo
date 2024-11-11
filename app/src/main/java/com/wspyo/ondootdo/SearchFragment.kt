8e01a7ba1a8fcd795c94d85b2084b451d3f15bd5
표원식 <pyowonsik@pyowonsig-ui-MacBookPro.local>
2024-11-11 23:31:00 +0900

    hotfix : modal 두번 뜨는 이슈 처리 및 폰트 적용

--- app/src/main/java/com/wspyo/ondootdo/SearchFragment.kt
+++ app/src/main/java/com/wspyo/ondootdo/SearchFragment.kt
@@ -3,6 +3,8 @@ package com.wspyo.ondootdo
 
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -63,33 +65,40 @@ class SearchFragment : Fragment() {
             // PlaceRVAdapter의 아이템 클릭 리스너 설정
             placeRVAdapter.itemClick = object : PlaceRVAdapter.ItemClick {
                 override fun onClick(view: View, position: Int) {
+                    // 이미 Dialog가 열려있는지 확인
+                    if (isDialogOpen) return // Dialog가 이미 열려있으면 처리하지 않음
 
-                    // 클릭 시 데이터 요청 (observe는 다시 설정하지 않음)
-                    // 클릭시 placeDetailViewModel의 weatherResponse 데이터 변경
-                    // -> 변경이 된 데이터를 감지하여 해당 데이터로 place detail Dialog open
-                    val document = placeResponse.documents[position]
-//                    Log.d("SearchFragment",  document.toString())
-
+                    // Dialog 열림 상태로 변경 후 일정 시간 후에 다시 false로 변경
                     isDialogOpen = true
+                    val document = placeResponse.documents[position]
 
                     placeName = document.place_name
-
                     placeDetailViewModel.getPlaceDetailWeather(
                         document.y.toDouble(),
                         document.x.toDouble(),
                         "dd488c2e7a32df4bc1e362d36f4a53ad"
                     )
+
+                    // Handler를 사용하여 500ms 이후에 다시 클릭 가능하도록 설정
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        isDialogOpen = false
+                    }, 500) // 500ms 동안 추가 클릭 방지
                 }
             }
+
+            // weatherResponse 관찰자는 최초에 한 번만 등록
+            placeDetailViewModel.weatherResponse.observe(viewLifecycleOwner) { temperature ->
+                if (isDialogOpen) {
+                    Log.d("SearchFragment","open")
+                    val dialogFragment = PlaceDetailsFragment.newInstance(temperature, placeName)
+                    dialogFragment.show(parentFragmentManager, "placeDetail")
+                }
+            }
+
+
+
         }
 
-        // weatherResponse 관찰자는 최초에 한 번만 등록
-        placeDetailViewModel.weatherResponse.observe(viewLifecycleOwner) { temperature ->
-            if(isDialogOpen){
-                val dialogFragment = PlaceDetailsFragment.newInstance(temperature,placeName)
-                dialogFragment.show(parentFragmentManager, "placeDetail")
-            }
-        }
 
         // 검색 버튼 클릭 시에만 데이터 요청
         binding.searchIcon.setOnClickListener {
